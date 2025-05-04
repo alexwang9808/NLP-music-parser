5eaa6b113bc60783ccede6c4ab9962f79f103b48
Alex Wang <awang2004j@gmail.com>
2025-05-04 00:27:40 -0500

    top 10 similar songs and sentiment analysis

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from find_song import *
+from FlagEmbedding import BGEM3FlagModel
+from pinecone import Pinecone
+from read_songs import get_sentiment
+
 
 # ----- Step 1: Get user input -----
 user_input = input("Enter song name (or 'Song by Artist'): ")
@@ -65,3 +69,48 @@ try:
         print("Cancelled.")
 except ValueError:
     print("Invalid input.")
+    
+# ----- Step 6: Get vector embedding of song -----
+vector_model = BGEM3FlagModel("BAAI/bge-m3")
+user_embedding = vector_model.encode(lyrics)["dense_vecs"]
+
+
+pc = Pinecone(api_key="d521ba70-bedc-4b48-9af8-82dda143a820")
+index = pc.Index("nlp-project")
+if lyrics:
+    # Embed and search
+    user_embedding = vector_model.encode(lyrics)["dense_vecs"]
+    results = index.query(
+    vector=user_embedding.tolist(),
+    top_k=20,  # Fetch more to account for duplicates
+    namespace="simple",
+    include_metadata=True
+    )
+
+    seen = set()
+    count = 0
+    selected_id = (title.lower(), artist.lower())
+
+    print("\nTop 10 most similar songs based on lyrics:")
+    for match in results["matches"]:
+        meta = match["metadata"]
+        song_id = (meta['song'].lower(), meta['artist'].lower())
+
+        if song_id in seen or song_id == selected_id:
+            continue
+
+        seen.add(song_id)
+        print(f"- '{meta['song']}' by {meta['artist']} (Score: {match['score']:.4f})")
+        count += 1
+
+        if count == 10:
+            break
+
+# ----- Step 7: Sentiment Analysis -----
+    sentiment = get_sentiment(lyrics)
+    print("\nSentiment Analysis:")
+    print(f"Positive: {sentiment['positive']:.2f}")
+    print(f"Neutral: {sentiment['neutral']:.2f}")
+    print(f"Negative: {sentiment['negative']:.2f}")
+    
+
